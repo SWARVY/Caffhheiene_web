268b3a352166642ffbad96b1abd848ef6e3121c5
swarvy <swarvy0826@naver.com>
2023-09-26 19:01:04 +0900

    fix : user.ts 삭제

--- caffhheiene/src/constants/user.ts
+++
@@ -1,6 +0,0 @@
-export const USER = Object.freeze({
-  id: 'Caffhheiene',
-  description:
-    'Frontend Developer, aiming for a Web Full Stack Developer.$I am learning using React.js / Next.js / JavaScript / TypeScript / TailwindCSS',
-  github: 'Visit my github portfolio!',
-});

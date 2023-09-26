6d9270759d6ef1edf46cfa5678b904d92e865111
swarvy <swarvy0826@naver.com>
2023-09-26 18:58:01 +0900

    feat : badge용 svg 이미지 생성 및 index.tsx로 관리

---
+++ caffhheiene/public/svgs/index.tsx
@@ -0,0 +1,25 @@
+import JavaScriptSVG from './javascript.svg';
+import TypeScriptSVG from './typescript.svg';
+import ReactSVG from './react.svg';
+import ReactQuerySVG from './reactquery.svg';
+import RecoilSVG from './recoil.svg';
+import TailwindcssSVG from './tailwindcss.svg';
+import NextJsSVG from './nextjs.svg';
+
+const JavascriptSvg = () => <JavaScriptSVG width="20px" height="20px" />;
+const TypescriptSvg = () => <TypeScriptSVG width="20px" height="20px" />;
+const ReactSvg = () => <ReactSVG width="20px" height="20px" />;
+const ReactQuerySvg = () => <ReactQuerySVG width="20px" height="20px" />;
+const RecoilSvg = () => <RecoilSVG width="20px" height="20px" />;
+const TailwindcssSvg = () => <TailwindcssSVG width="20px" height="20px" />;
+const NextJsSvg = () => <NextJsSVG width="20px" height="20px" />;
+
+export {
+  JavascriptSvg,
+  TypescriptSvg,
+  ReactSvg,
+  ReactQuerySvg,
+  RecoilSvg,
+  TailwindcssSvg,
+  NextJsSvg,
+};

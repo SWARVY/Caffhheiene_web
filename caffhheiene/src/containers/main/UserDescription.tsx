8ec32390ac9c7646fc64c3819f199cca873ba741
swarvy <swarvy0826@naver.com>
2023-09-26 22:44:52 +0900

    feat : UserDescription 레이아웃 수정 및 디자인 수정

--- caffhheiene/src/containers/main/UserDescription.tsx
+++ caffhheiene/src/containers/main/UserDescription.tsx
@@ -4,25 +4,26 @@ import React from 'react';
 
 export default function UserDescription() {
   return (
-    <>
-      <div className="flex w-full text-start">
-        <h1 className="text-4xl md:text-5xl drop-shadow-xl">{USER.id}</h1>
+    <div className="grid w-full">
+      <div className="flex w-full text-start gap-y-10 md:gap-y-0">
+        <h1 className="text-5xl drop-shadow-xl">{USER.id}</h1>
       </div>
-      <div className="flex-col md:grid md:grid-cols-2 w-full">
-        <div className="grid md:p-4">
+      <div className="grid md:grid-cols-5 w-full">
+        <div className="md:p-4 md:col-span-3 space-y-3">
+          <h1 className="text-4xl">Introduce</h1>
           <p className="text-slate-500">{USER.description}</p>
         </div>
-        <div className="grid md:p-4 gap-10">
+        <div className="grid md:p-4 md:col-span-2 gap-10">
           <div className="space-y-3">
             <h1 className="text-4xl">Currently Learning</h1>
             <Badges type="TECH" />
           </div>
           <div className="space-y-3">
-            <h1 className="text-4xl">Contact ME</h1>
+            <h1 className="text-4xl">Contact</h1>
             <Badges type="SNS" />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }

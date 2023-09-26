5d4a5add63627780c3ef43982ba922a881ab9012
swarvy <swarvy0826@naver.com>
2023-09-26 22:44:26 +0900

    feat : card 컴포넌트 스타일링 수정

--- caffhheiene/src/components/Card.tsx
+++ caffhheiene/src/components/Card.tsx
@@ -13,10 +13,10 @@ export default function Card({ data }: CardComponentProps) {
   return (
     <div className="grid grid-cols-1 rounded-lg bg-white_hover">
       <img className="rounded-t-lg" src="http://via.placeholder.com/640x480" alt="sample" />
-      <div className="flex flex-col p-4">
+      <div className="flex flex-col p-3">
         <div className="flex p-2">
-          <CalendarIcon className="w-3 h-full items-center mr-2" />
-          <p className="text-xs">{data.date}</p>
+          <CalendarIcon className="w-3 h-3 items-center mr-2" />
+          <span className="text-xs">{data.date}</span>
         </div>
         <h1 className="p-2 text-xl">{data.title}</h1>
       </div>

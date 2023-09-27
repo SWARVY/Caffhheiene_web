7471356ce088dc37d896cc32535077bd7a7bcd78
swarvy <swarvy0826@naver.com>
2023-09-27 14:36:05 +0900

    feat : posts 페이지 폰트, 디자인 일부 수정

--- caffhheiene/src/app/posts/page.tsx
+++ caffhheiene/src/app/posts/page.tsx
@@ -3,13 +3,13 @@ import { TITLE } from '@/constants/title';
 
 export default function posts() {
   return (
-    <div className="flex flex-col mt-4 z-10 w-full items-center justify-center lg:flex">
+    <div className="flex-col w-full space-y-3 items-center justify-center">
       <div className="flex w-full text-start">
-        <h1 className="text-4xl md:text-5xl font-MBC_1961_M drop-shadow-xl">{TITLE.posts}</h1>
+        <h1 className="text-4xl md:text-5xl drop-shadow-xl">{TITLE.posts}</h1>
       </div>
-      <div className="flex w-full justify-start mt-6 gap-x-5">
+      <div className="flex w-full justify-start  gap-x-5">
         {FILTERS.map((filter) => (
-          <div className="flex transition ease-in rounded-lg bg-white_hover hover:bg-white_hover_weight pr-2 pl-2 pt-1 pb-1 font-LINE_Rg">
+          <div className="flex transition ease-in rounded-lg bg-white_hover hover:bg-white_hover_weight pr-2 pl-2 pt-1 pb-1">
             <h3 className="mr-1">{filter.name}</h3>
             <p className="text-xs">(0)</p>
           </div>

c164ab0ed22e3dd6a3dd340bfb32dd1fd1089bb9
swarvy <swarvy0826@naver.com>
2023-09-28 00:50:41 +0900

    feat : UserDescription 파트 디자인 수정 (제목 앞 이모티콘 추가, 스타일 일부 수정)

--- caffhheiene/src/constants/user.tsx
+++ caffhheiene/src/constants/user.tsx
@@ -16,6 +16,10 @@ export const USER_CAREER = Object.freeze([
     name: '경기대학교 InQ FE Developer & Tutor',
   },
   {
+    date: '2023. 01. ~ 2023. 02.',
+    name: '42SEOUL PISCINER',
+  },
+  {
     date: '2023. 03. ~ 2023. 10. 03.',
     name: '42SEOUL CADET',
   },
@@ -24,25 +28,25 @@ export const USER_CAREER = Object.freeze([
 export const PREVIOUS_PROJECTS = [
   {
     link: 'https://github.com/SWARVY/KUBS',
-    name: 'KUBS (경기대학교 종합 예약 시스템)',
+    name: '⏱️ KUBS (경기대학교 종합 예약 시스템)',
     description: '학교 시설물들에 대한 대여 시스템을 웹에서 제공하는 서비스를 개발하였습니다',
     stack: [BADGES.react, BADGES.redux, BADGES.javascript],
   },
   {
     link: 'https://github.com/SWARVY/Ezipnaezip_web',
-    name: '이집내집 (AI 인테리어 디자인 사이트)',
+    name: '🏠 이집내집',
     description: '웹에서 DALL.E를 사용한 AI 인테리어 디자인 / 커뮤니티 서비스를 개발하였습니다',
     stack: [BADGES.react, BADGES.recoil, BADGES.reactQuery, BADGES.javascript, BADGES.tailwindcss],
   },
   {
     link: 'https://github.com/TeamCommerce',
-    name: '팀커머스 (이커머셜 웹 사이트)',
+    name: '💸 팀커머스',
     description: '이 커머스 웹 플랫폼(피팅노트)을 클론 코딩하였습니다',
     stack: [BADGES.react, BADGES.recoil, BADGES.reactQuery, BADGES.typescript, BADGES.tailwindcss],
   },
   {
     link: 'https://github.com/SWARVY/Caffhheiene_web',
-    name: 'Caffhheiene (개인 기술블로그)',
+    name: '⚙️ Caffhheiene (개인 기술블로그)',
     description: '지금 보고계시는 이 블로그를 직접 만들었습니다',
     stack: [BADGES.nextjs, BADGES.recoil, BADGES.reactQuery, BADGES.typescript, BADGES.tailwindcss],
   },
--- caffhheiene/src/containers/resume/UserDescription.tsx
+++ caffhheiene/src/containers/resume/UserDescription.tsx
@@ -13,7 +13,7 @@ export default function UserDescription() {
           <div className="space-y-3">
             <h1 className="text-4xl">{TITLE.introduce}</h1>
             <p className="text-lg text-slate-500">{USER.description}</p>
-            <div className="flex-col w-2/3 p-2">
+            <div className="flex-col w-3/4 p-2 space-y-1">
               {USER_CAREER.map((data, idx) => (
                 <div key={idx} className="grid grid-cols-2 gap-x-3">
                   <h1 className="text-gray-500">{data.date}</h1>

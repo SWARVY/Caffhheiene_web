1d669ebbedc1f018edaf77e49be004af1038c475
swarvy <swarvy0826@naver.com>
2023-09-27 11:06:04 +0900

    feat : root layout 수정

--- caffhheiene/src/app/layout.tsx
+++ caffhheiene/src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="flex min-h-screen flex-col font-MABINOGI_Classic items-center justify-between p-24">
+      <body className="flex-col p-40 lg:pr-80 lg:pl-80 font-MABINOGI_Classic">
         <TopNavigator />
         {children}
       </body>

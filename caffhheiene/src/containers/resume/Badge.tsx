c4d1c3a4c9d4d34380272d92cd5bf88edda3d40f
swarvy <swarvy0826@naver.com>
2023-09-27 15:06:51 +0900

    feat : badge hover 효과 추가 (hover시 어두워지도록)

--- caffhheiene/src/containers/resume/Badge.tsx
+++ caffhheiene/src/containers/resume/Badge.tsx
@@ -8,7 +8,10 @@ interface BadgeProps {
 
 export default function Badge({ svgImage, name, color }: BadgeProps) {
   return (
-    <div className="flex p-2 items-center rounded-sm" style={{ backgroundColor: `${color}` }}>
+    <div
+      className="flex transition ease-in duration-150 p-2 items-center rounded-sm hover:brightness-90"
+      style={{ backgroundColor: `${color}` }}
+    >
       <div className="w-full mr-2">{svgImage}</div>
       <h1 className={`${color === '#000000' ? 'text-white' : 'text-black'} text-sm`}>{name}</h1>
     </div>

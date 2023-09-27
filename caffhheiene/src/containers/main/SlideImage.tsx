8f251fb54ac48280376d94c56273d4ba38ed8f9f
swarvy <swarvy0826@naver.com>
2023-09-27 14:43:18 +0900

    feat : hover시 어두워지는 기능을 tailwind 자체속성으로 변경

--- caffhheiene/src/containers/main/SlideImage.tsx
+++ caffhheiene/src/containers/main/SlideImage.tsx
@@ -8,12 +8,11 @@ export default function SlideImage({ src, title, subtitle }: SlideImageProps) {
   return (
     <div className="relative group">
       <img
-        className="w-full h-[27rem] object-fill relative hover:bg-blend-darken"
+        className="transition ease-in-out duration-300 w-full h-[27rem] object-fill relative group-hover:brightness-75"
         src={src}
         alt="slideImage"
         loading="lazy"
       />
-      <div className="absolute transition ease-in-out duration-300 z-10 top-0 left-0 w-[90rem] h-[90rem] bg-black opacity-0 group-hover:opacity-30" />
       <div className="absolute transition ease-in-out duration-300 opacity-0 group-hover:opacity-100 z-20 bottom-0 right-0 text-white text-right p-10">
         <h1 className="text-4xl">{title}</h1>
         <h2 className="text-xl">{subtitle}</h2>

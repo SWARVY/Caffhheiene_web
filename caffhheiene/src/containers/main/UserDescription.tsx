b9410f88a83b2e941227828c50f732ee176f671e
swarvy <swarvy0826@naver.com>
2023-09-27 10:01:44 +0900

    feat : (임시) content 영역 생성 및 테스트

--- caffhheiene/src/containers/main/UserDescription.tsx
+++ caffhheiene/src/containers/main/UserDescription.tsx
@@ -4,6 +4,7 @@ import Badges from '@/containers/main/Badges';
 import ImageSlider from '@/containers/main/ImageSlider';
 import SlideImage from '@/containers/main/SlideImage';
 import { TestCarouselData } from '@/containers/main/TestSources/TestCarouselData';
+import { TestContentData } from '@/containers/main/TestSources/TestContentData';
 
 export default function UserDescription() {
   return (
@@ -12,12 +13,20 @@ export default function UserDescription() {
         <h1 className="text-5xl drop-shadow-xl">{USER.id}</h1>
       </div>
       <div className="grid md:grid-cols-5 w-full">
-        <div className="md:p-4 md:col-span-3 space-y-3">
+        <div className="flex w-full md:p-4 md:col-span-3 space-y-3 gap-x-5">
           <ImageSlider>
             {TestCarouselData.map((data) => (
               <SlideImage src={data.src} title={data.title} subtitle={data.subtitle} />
             ))}
           </ImageSlider>
+          {/*<div className="flex-col overflow-y-scroll space-y-3">*/}
+          {/*  {TestContentData.map((data) => (*/}
+          {/*    <div className="flex-col w-full p-2 border-b border-gray-600">*/}
+          {/*      <h3 className="text-gray-500">{data.date}</h3>*/}
+          {/*      <h1 className="text-lg">{data.title}</h1>*/}
+          {/*    </div>*/}
+          {/*  ))}*/}
+          {/*</div>*/}
         </div>
         <div className="grid md:p-4 md:col-span-2 gap-10">
           <div className="space-y-3">

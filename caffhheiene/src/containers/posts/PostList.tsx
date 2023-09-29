7f3632386a41aded54dfca4733b0da678aa6a69e
swarvy <swarvy0826@naver.com>
2023-09-29 20:15:57 +0900

    feat : public/blog 내의 마크다운을 파싱하여 리스트로 출력하는 기능 구현

--- caffhheiene/src/containers/posts/PostList.tsx
+++ caffhheiene/src/containers/posts/PostList.tsx
@@ -1,42 +1,48 @@
 import { USER } from '@/constants/user';
-import getSortedPostData from '@/utils/getSortedPostData';
+import { getSortedPostData } from '@/utils/getPostData';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function PostList() {
-  const posts: Array<{ [p: string]: any; id: string }> = getSortedPostData();
+  const posts = getSortedPostData();
 
   return (
     <ul className="grid gap-8">
       {posts.map((post, idx) => (
-        <li key={idx} className="grid gap-4 p-10 rounded-md border">
-          <div className="grid gap-3">
-            <div className="grid gap-2">
-              <h1 className="text-xl">{post?.title}</h1>
-              <p className="text-md text-gray-400">{post?.description}</p>
-            </div>
-            <div className="flex items-center gap-3">
-              <Image
-                className="rounded-full w-8 h-8"
-                src="/images/Profile_glassed_icon.jpg"
-                alt="profile"
-                width={32}
-                height={32}
-              />
-              <h1 className="text-lg">{USER.name}</h1>
-              <h1 className="text-md text-gray-400">{post?.date}</h1>
-            </div>
-          </div>
-          <div className="flex gap-2">
-            {post?.category.map((tag: string, idx: number) => (
-              <div
-                key={idx}
-                className="flex bg-slate-400 text-white pt-0.5 pb-0.5 pl-2 pr-2 rounded-md "
-              >
-                {tag}
+        <Link key={idx} href={`/posts/${idx + 1}`}>
+          <li
+            key={idx}
+            className="grid transition ease-in duration-75 gap-4 p-10 rounded-md border hover:border-blue-500 hover:-translate-y-1"
+          >
+            <div className="grid gap-3">
+              <div className="grid gap-2">
+                <h1 className="text-xl font-bold">{post.data.title}</h1>
+                <p className="text-md text-gray-400">{post.data.description}</p>
               </div>
-            ))}
-          </div>
-        </li>
+              <div className="flex items-center gap-3">
+                <Image
+                  className="rounded-full w-7 h-7"
+                  src="/images/Profile_glassed_icon.jpg"
+                  alt="profile"
+                  width={32}
+                  height={32}
+                />
+                <h1 className="text-md">{USER.name}</h1>
+                <h1 className="text-sm text-gray-400">{post.data.date}</h1>
+              </div>
+            </div>
+            <div className="flex gap-2">
+              {post.data.category.map((tag: string, idx: number) => (
+                <div
+                  key={idx}
+                  className="flex bg-slate-400 text-white pt-0.5 pb-0.5 pl-2 pr-2 rounded-md "
+                >
+                  {tag}
+                </div>
+              ))}
+            </div>
+          </li>
+        </Link>
       ))}
     </ul>
   );

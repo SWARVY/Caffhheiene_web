b15c739d4cc7ea1534703d6ef6ff8e469943d794
swarvy <swarvy0826@naver.com>
2023-09-29 21:02:26 +0900

    fix : Post 글자 시인성 향상을 위한 font-bold 설정

--- caffhheiene/src/containers/posts/PostTags.tsx
+++ caffhheiene/src/containers/posts/PostTags.tsx
@@ -5,7 +5,7 @@ export default function PostTags() {
   return (
     <div className="flex-col w-full space-y-3 items-center justify-center">
       <div className="flex w-full text-start">
-        <h1 className="text-4xl md:text-5xl drop-shadow-xl">{TITLE.posts}</h1>
+        <h1 className="text-4xl md:text-5xl drop-shadow-xl font-bold">{TITLE.posts}</h1>
       </div>
       <div className="flex w-full justify-start  gap-x-5">
         {FILTERS.map((filter, idx) => (

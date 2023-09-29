c2ef08c6e4b43506dfd82a5e17a1c7930509f1c7
swarvy <swarvy0826@naver.com>
2023-09-29 23:27:41 +0900

    feat : projects 페이지 디자인 통일 및 하드코딩파트 상수분리

--- caffhheiene/src/containers/projects/PreviousProjects.tsx
+++ caffhheiene/src/containers/projects/PreviousProjects.tsx
@@ -1,19 +1,25 @@
+import { MAIN_TITLE, SUB_TITLE } from '@/constants/title';
 import { PREVIOUS_PROJECTS } from '@/constants/user';
 import Badge from '@/components/Badge';
 
 export default function PreviousProjects() {
   return (
     <div className="grid gap-10">
-      <h1>* 제목 클릭 시 새 창에서 깃허브 레포지토리가 열립니다</h1>
+      <div className="flex w-full justify-center md:justify-start md:gap-y-0 gap-x-4">
+        <h1 className="flex text-6xl md:text-5xl drop-shadow-xl font-bold">
+          {MAIN_TITLE.projects}
+        </h1>
+      </div>
+      <h1 className="p-3 rounded-md border border-black">{SUB_TITLE.notice}</h1>
       {PREVIOUS_PROJECTS.map((data, idx) => (
         <div key={idx} className="grid gap-3">
           <a href={data.link} target="_blank" rel="noreferrer">
-            <h1 className="text-3xl">{data.name}</h1>
+            <h1 className="text-2xl font-bold">{data.name}</h1>
           </a>
           <h2 className="">{data.description}</h2>
           <div className="border border-black rounded-md overflow-hidden">
             <div className="bg-slate-300">
-              <h1 className="p-3">사용 기술스택</h1>
+              <h1 className="p-3 font-bold">{SUB_TITLE.techStacks}</h1>
             </div>
             <hr className="border-black" />
             <div className="grid grid-cols-2 md:grid-cols-3 p-3 gap-2">

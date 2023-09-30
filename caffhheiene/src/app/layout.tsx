cb3ebeb9e6161f623f1fc3ebd37826de668ba1e6
swarvy <swarvy0826@naver.com>
2023-09-30 22:25:11 +0900

    feat : RootLayout 반응형 좌우 padding 수정

--- caffhheiene/src/app/layout.tsx
+++ caffhheiene/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className="flex-col w-full font-MABINOGI_Classic">
         <TopNavigator />
         <Client>
-          <div className="p-5 pt-40 sm:pr-20 sm:pl-20 md:pl-40 md:pr-40 lg:pr-60 lg:pl-60 ">
+          <div className="p-5 pt-40 sm:pr-20 sm:pl-20 md:pl-40 md:pr-40 lg:pr-60 lg:pl-60 xl:pr-80 xl:pl-80 ">
             {children}
           </div>
         </Client>

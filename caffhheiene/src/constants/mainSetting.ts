9a3776fc4ff3a1bc305d8754f7c967034156dd2e
swarvy <swarvy0826@naver.com>
2023-09-30 22:25:53 +0900

    feat : 루트 페이지에서 사용될 상수 생성

---
+++ caffhheiene/src/constants/mainSetting.ts
@@ -0,0 +1,18 @@
+export const MAIN_SETTING = {
+  recentlyPostDataAmount: 4,
+};
+
+export const MAIN_CAROUSEL_SETTING = [
+  {
+    src: '/blogImages/image00.png',
+    title: '제 블로그에 오신걸 환영합니다!',
+    subtitle: '이 사이트는 Next.js로 만들어졌습니다.',
+    link: '',
+  },
+  {
+    src: '/blogImages/image01.jpg',
+    title: '제 블로그 디자인 어떤가요?',
+    subtitle: '이 사이트는 Vercel을 통해 호스팅되고있습니다.',
+    link: '',
+  },
+];

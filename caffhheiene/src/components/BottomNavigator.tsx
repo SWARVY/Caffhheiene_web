9f25b32d6ea0a14a018c6da06626ff9292ca962d
swarvy <swarvy0826@naver.com>
2023-09-30 11:58:30 +0900

    feat : BottomNavigator.tsx 반응형 레이아웃 일부 수정

--- caffhheiene/src/components/BottomNavigator.tsx
+++ caffhheiene/src/components/BottomNavigator.tsx
@@ -2,7 +2,7 @@ import { BOTTOM_NAVBAR } from '@/constants/navbar';
 
 export default function BottomNavigator() {
   return (
-    <div className="p-20 text-center">
+    <div className="pt-20 pb-20 md:pr-20 md:pl-20 text-center">
       <h1>{BOTTOM_NAVBAR.copyright}</h1>
     </div>
   );

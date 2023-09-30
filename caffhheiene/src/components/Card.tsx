86b179dd20102f3f7db8f38b70d11d558c367601
swarvy <swarvy0826@naver.com>
2023-09-30 22:28:25 +0900

    feat : Card 컴포넌트가 gray-matter로 분리된 front-matter의 데이터를 읽어오도록 수정 / 디자인 일부 수정

--- caffhheiene/src/components/Card.tsx
+++ caffhheiene/src/components/Card.tsx
@@ -1,35 +1,38 @@
 import { CalendarIcon } from '@heroicons/react/20/solid';
 import { ChatBubbleOvalLeftIcon } from '@heroicons/react/24/outline';
-
-interface CardData {
-  date: string;
-  title: string;
-}
+import { type GrayMatterFile } from 'gray-matter';
+import Link from 'next/link';
 
 interface CardComponentProps {
-  data: CardData;
+  id: number;
+  post: GrayMatterFile<string>;
 }
 
-export default function Card({ data }: CardComponentProps) {
+export default function Card({ id, post }: CardComponentProps) {
   return (
-    <div className="grid grid-cols-1 transition ease-in rounded-md border-2 bg-white_hover overflow-hidden hover:-translate-y-2">
-      <img className="border-b" src="http://via.placeholder.com/640x480" alt="sample" />
-      <div className="flex flex-col p-3 bg-white">
-        <div className="grid grid-cols-1 p-2 items-center">
-          <div className="flex h-full justify-items-end">
-            <CalendarIcon className="w-3 h-3 items-center mr-2" />
-            <span className="text-xs">{data.date}</span>
+    <Link href={`/posts/${id + 1}`}>
+      <div className="grid grid-cols-1 transition ease-in rounded-md border-2 bg-white_hover overflow-hidden hover:-translate-y-2">
+        <img className="border-b" src={post.data.thumbnail} alt="sample" />
+        <div className="flex flex-col p-3 bg-white">
+          <div className="grid grid-cols-1 p-2 items-center">
+            <div className="flex h-full justify-items-end">
+              <CalendarIcon className="w-3 h-3 items-center mr-2" />
+              <span className="text-xs">{post.data.date}</span>
+            </div>
           </div>
-        </div>
-        <h1 className="p-2 text-xl">{data.title}</h1>
-        <hr />
-        <div className="grid grid-cols-1 mt-2 p-2 items-center">
-          <div className="flex h-full justify-items-end">
-            <ChatBubbleOvalLeftIcon className="w-3 h-3 items-center mr-2" />
-            <span className="text-xs">0</span>
+          <div className="grid p-2 gap-1">
+            <h1 className="text-xl font-bold">{post.data.title}</h1>
+            <p className="text-md text-gray-500">{post.data.description}</p>
+          </div>
+          <hr />
+          <div className="grid grid-cols-1 mt-2 p-2 items-center">
+            <div className="flex h-full justify-items-end">
+              <ChatBubbleOvalLeftIcon className="w-3 h-3 items-center mr-2" />
+              <span className="text-xs">0</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }

7ae297b639d474161f0f7400286bb3aeac78ccbe
swarvy <swarvy0826@naver.com>
2023-10-01 13:06:46 +0900

    feat : subtitle에 hover시 밑줄이 생기도록 구현

--- caffhheiene/src/containers/main/RecentlyPost.tsx
+++ caffhheiene/src/containers/main/RecentlyPost.tsx
@@ -8,7 +8,9 @@ export default function RecentlyPost() {
 
   return (
     <div className="flex-col w-full mt-3">
-      <h1 className="text-4xl font-bold italic">{SUB_TITLE.recentlyPosts}</h1>
+      <h1 className="text-4xl font-bold italic hover:underline hover:underline-offset-1">
+        {SUB_TITLE.recentlyPosts}
+      </h1>
       <div className="mt-8 grid grid-cols-2 gap-4">
         {posts.map((post, idx) => (
           <Card key={idx} id={idx} post={post} />

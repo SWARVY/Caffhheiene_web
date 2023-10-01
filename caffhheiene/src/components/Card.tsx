126f1ff8fe615aeb5a0f16fa3e3f42dc140e4fce
swarvy <swarvy0826@naver.com>
2023-10-01 23:00:40 +0900

    feat : Card.tsx 불필요한 css 설정 제거 후 디자인 변경

--- caffhheiene/src/components/Card.tsx
+++ caffhheiene/src/components/Card.tsx
@@ -11,7 +11,7 @@ interface CardComponentProps {
 export default function Card({ id, post }: CardComponentProps) {
   return (
     <Link href={`/posts/${id + 1}`}>
-      <div className="grid grid-cols-1 transition ease-in rounded-md border-2 bg-white_hover overflow-hidden hover:-translate-y-2">
+      <div className="grid transition ease-in rounded-md bg-white_hover overflow-hidden shadow-lg hover:-translate-y-2">
         <img className="border-b" src={post.data.thumbnail} alt="sample" />
         <div className="flex flex-col p-3 bg-white">
           <div className="grid grid-cols-1 p-2 items-center">

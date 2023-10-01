7c40823fd016e5c438de0d893c41f004609c897a
swarvy <swarvy0826@naver.com>
2023-10-02 02:51:12 +0900

    feat : FrozenRouter를 사용하여 page transition으로 일어나던 버그 수정

--- caffhheiene/src/app/client.tsx
+++ caffhheiene/src/app/client.tsx
@@ -1,30 +1,34 @@
 'use client';
 import { AnimatePresence, motion } from 'framer-motion';
+import { LayoutRouterContext } from 'next/dist/shared/lib/app-router-context';
 import { usePathname } from 'next/navigation';
-import { type ReactNode } from 'react';
+import { type PropsWithChildren, type ReactNode, useContext, useRef } from 'react';
 
 interface TransitionDivProps {
   children: ReactNode;
+}
+
+function FrozenRouter(props: PropsWithChildren) {
+  const context = useContext(LayoutRouterContext);
+  const frozen = useRef(context).current;
+
+  return (
+    <LayoutRouterContext.Provider value={frozen}>{props.children}</LayoutRouterContext.Provider>
+  );
 }
 
 export default function Client({ children }: TransitionDivProps) {
   const pathName = usePathname();
 
   return (
-    <AnimatePresence
-      initial
-      mode="wait"
-      onExitComplete={() => {
-        window.scrollTo(0, 0);
-      }}
-    >
+    <AnimatePresence initial mode="wait">
       <motion.div
         key={pathName}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
       >
-        {children}
+        <FrozenRouter>{children}</FrozenRouter>
       </motion.div>
     </AnimatePresence>
   );

1d021475ffe29f7fb9a47e71b8b5be0ecf902297
swarvy <swarvy0826@naver.com>
2023-10-01 13:04:39 +0900

    feat : Tag.tsx의 디자인 변경

--- caffhheiene/src/containers/posts/Tag.tsx
+++ caffhheiene/src/containers/posts/Tag.tsx
@@ -1,15 +1,10 @@
-import { BADGES } from '@/constants/badge';
-
 interface TagProps {
   tag: string;
 }
 
 export default function Tag({ tag }: TagProps) {
   return (
-    <div
-      className="flex text-white pt-0.5 pb-0.5 pl-2 pr-2 rounded-md"
-      style={{ backgroundColor: `${BADGES.typescript.color}` }}
-    >
+    <div className="flex transition ease-in bg-white_hover hover:bg-white_hover_weight pt-0.5 pb-0.5 pl-2 pr-2 rounded-md">
       {tag}
     </div>
   );

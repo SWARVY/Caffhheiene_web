b064726d12ef2caeaa32a43bd51245952cccd4bc
swarvy <swarvy0826@naver.com>
2023-10-02 22:37:31 +0900

    feat : 이전글, 다음글로 이동하는 버튼 추가

--- caffhheiene/src/containers/posts/PostDetail.tsx
+++ caffhheiene/src/containers/posts/PostDetail.tsx
@@ -1,38 +1,40 @@
 import { USER } from '@/constants/user';
 import PostWriterInfo from '@/containers/posts/PostWriterInfo';
+import PrevNextButton from '@/containers/posts/PrevNextButton';
 import Tag from '@/containers/posts/Tag';
 import TailwindMarkdownRenderer from '@/containers/posts/TailwindMarkdownRenderer';
 import { getDetailPostData } from '@/utils/getPostData';
 import Image from 'next/image';
 
 export default function PostDetail({ id }: { id: string }) {
-  const postData = getDetailPostData(Number(id));
+  const { prev, curr, next } = getDetailPostData(Number(id));
 
   return (
-    <div className="grid gap-20">
+    <div className="grid gap-10">
       <div className="grid gap-6">
-        <h1 className="text-5xl font-bold">{postData.data.title}</h1>
+        <h1 className="text-5xl font-bold">{curr.data.title}</h1>
         <div className="grid p-1 gap-4">
           <div className="flex items-center gap-3">
             <h1 className="text-lg">{USER.name}</h1>
-            <h1 className="text-md text-gray-400">{postData.data.date}</h1>
+            <h1 className="text-md text-gray-400">{curr.data.date}</h1>
           </div>
           <div className="flex gap-2">
-            {postData.data.category.map((tag: string, idx: number) => (
+            {curr.data.category.map((tag: string, idx: number) => (
               <Tag key={idx} tag={tag} />
             ))}
           </div>
         </div>
         <Image
           className="rounded-md w-full"
-          src={postData.data.thumbnail}
+          src={curr.data.thumbnail}
           alt="thumnail"
           width={1024}
           height={1024}
         />
       </div>
-      <TailwindMarkdownRenderer content={postData.content} />
+      <TailwindMarkdownRenderer content={curr.content} />
       <PostWriterInfo />
+      <PrevNextButton id={Number(id)} prev={prev} next={next} />
     </div>
   );
 }

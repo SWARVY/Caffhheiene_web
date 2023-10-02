992b7dd067f176610fbd2d4041621e2b09f9126d
swarvy <swarvy0826@naver.com>
2023-10-02 22:36:36 +0900

    feat : giscus를 이용한 댓글 시스템 구현

---
+++ caffhheiene/src/containers/posts/PostComments.tsx
@@ -0,0 +1,21 @@
+'use client';
+import Giscus from '@giscus/react';
+
+export default function PostComments() {
+  return (
+    <Giscus
+      id="comments"
+      repo="SWARVY/Caffhheiene_web"
+      repoId="R_kgDOJyONKw"
+      category="Announcements"
+      categoryId="DIC_kwDOJyONK84CZyFe"
+      mapping="pathname"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="light_high_contrast"
+      lang="en"
+      loading="lazy"
+    />
+  );
+}

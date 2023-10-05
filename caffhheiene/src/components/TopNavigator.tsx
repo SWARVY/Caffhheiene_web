d10f87538ffa83bd5000315842e4c3802ee1885d
swarvy <swarvy0826@naver.com>
2023-10-05 23:44:54 +0900

    fix : 반응형 스타일 일부 수정

--- caffhheiene/src/components/TopNavigator.tsx
+++ caffhheiene/src/components/TopNavigator.tsx
@@ -7,18 +7,18 @@ import { BearSmileSVG } from '../../public/svgs';
 export default function TopNavigator() {
   return (
     <nav className="fixed left-0 top-0 z-10 w-full flex-col items-center">
-      <div className="flex w-full bg-white bg-opacity-60 px-14 py-8">
+      <div className="flex w-full bg-white bg-opacity-60 px-5 py-8 md:px-14">
         <div className="flex w-full justify-between font-bold">
-          <div className="flex h-full items-center gap-x-2 md:gap-x-5">
+          <div className="flex h-full items-center gap-x-3 md:gap-x-6">
             <Link href="/">
-              <BearSmileSVG className="h-8 w-8 drop-shadow-md" />
+              <BearSmileSVG className="h-8 w-8 drop-shadow-md transition duration-150 ease-in hover:animate-pulse" />
             </Link>
             {TOP_NAVBAR.tags.map(({ tag, link }, idx) => (
               <Link
                 key={idx}
-                className="h-full items-center rounded-lg p-2 drop-shadow-md transition ease-in hover:bg-white_hover"
+                className="flex h-full items-center rounded-lg p-2 text-lg drop-shadow-md transition ease-in hover:bg-white_hover"
                 href={link}>
-                {tag}
+                <h1>{tag}</h1>
               </Link>
             ))}
           </div>

615532667e8e76b9a3b05e33df04a5d9478e887a
swarvy <swarvy0826@naver.com>
2023-10-11 11:17:46 +0900

    fix : UserCareer 출력이 잘리지 않도록 수정

--- caffhheiene/src/containers/info/UserCareer.tsx
+++ caffhheiene/src/containers/info/UserCareer.tsx
@@ -2,7 +2,7 @@ import { USER_CAREER } from '@/constants/user'
 
 export default function UserCareer() {
   return (
-    <div className="w-full flex-col space-y-1 p-2 md:w-3/4">
+    <div className="w-full flex-col space-y-1 p-2">
       {USER_CAREER.map((data, idx) => (
         <div key={idx} className="grid grid-cols-2 gap-x-3">
           <h1 className="text-gray-500">{data.date}</h1>

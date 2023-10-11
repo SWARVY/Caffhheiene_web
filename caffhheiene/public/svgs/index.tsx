f7a4211859569afd29533fccd9afb8c5e11d1ce3
swarvy <swarvy0826@naver.com>
2023-10-11 14:50:44 +0900

    feat : 필요 svg 추가 및 svg 색상을 흰색으로 통일

--- caffhheiene/public/svgs/index.tsx
+++ caffhheiene/public/svgs/index.tsx
@@ -1,24 +1,25 @@
-import JavaScriptSVG from './javascript.svg';
-import TypeScriptSVG from './typescript.svg';
-import ReactSVG from './react.svg';
-import ReactQuerySVG from './reactquery.svg';
-import RecoilSVG from './recoil.svg';
-import TailwindcssSVG from './tailwindcss.svg';
-import NextJsSVG from './nextjs.svg';
-import InstagramSVG from './instagram.svg';
-import GithubSVG from './github.svg';
-import NaverSVG from './naver.svg';
-import Html5SVG from './html5.svg';
-import CSS3SVG from './css3.svg';
-import ReduxSVG from './redux.svg';
-import FramerSVG from './framer.svg';
-import GitSVG from './git.svg';
-import AxiosSVG from './axios.svg';
-import BearSmileSVG from './bearsmile.svg';
-import GamepadSVG from './gamepad.svg';
-import MarkdownSVG from './markdown.svg';
-import BookReadSVG from './bookread.svg';
-import UserSmileSVG from './usersmile.svg';
+import JavaScriptSVG from './javascript.svg'
+import TypeScriptSVG from './typescript.svg'
+import ReactSVG from './react.svg'
+import ReactQuerySVG from './reactquery.svg'
+import RecoilSVG from './recoil.svg'
+import TailwindcssSVG from './tailwindcss.svg'
+import NextJsSVG from './nextjs.svg'
+import InstagramSVG from './instagram.svg'
+import GithubSVG from './github.svg'
+import TistorySVG from './tistory.svg'
+import NaverSVG from './naver.svg'
+import Html5SVG from './html5.svg'
+import CSS3SVG from './css3.svg'
+import ReduxSVG from './redux.svg'
+import FramerSVG from './framer.svg'
+import GitSVG from './git.svg'
+import AxiosSVG from './axios.svg'
+import BearSmileSVG from './bearsmile.svg'
+import GamepadSVG from './gamepad.svg'
+import MarkdownSVG from './markdown.svg'
+import BookReadSVG from './bookread.svg'
+import UserSmileSVG from './usersmile.svg'
 
 export {
   JavaScriptSVG,
@@ -31,6 +32,7 @@ export {
   InstagramSVG,
   GithubSVG,
   NaverSVG,
+  TistorySVG,
   Html5SVG,
   CSS3SVG,
   ReduxSVG,
@@ -42,4 +44,4 @@ export {
   MarkdownSVG,
   BookReadSVG,
   UserSmileSVG,
-};
+}
--- caffhheiene/src/components/Badge.tsx
+++ caffhheiene/src/components/Badge.tsx
@@ -1,4 +1,3 @@
-import getTextColorByBackgroundColor from '@/utils/getTextColorByBackgroundColor'
 import { type ReactNode } from 'react'
 
 interface BadgeProps {
@@ -8,14 +7,12 @@ interface BadgeProps {
 }
 
 export default function Badge({ svgImage, name, color }: BadgeProps) {
-  const adaptiveColor = getTextColorByBackgroundColor(color)
-
   return (
     <div
       className="flex items-center rounded-sm p-2 shadow-lg transition duration-150 ease-in hover:brightness-90"
       style={{ backgroundColor: `${color}` }}>
-      <div className={`mr-2 w-full fill-${adaptiveColor}`}>{svgImage}</div>
-      <h1 className={`text-${adaptiveColor} text-sm`}>{name}</h1>
+      <div className="mr-2 w-full fill-white">{svgImage}</div>
+      <h1 className="text-sm text-white">{name}</h1>
     </div>
   )
 }

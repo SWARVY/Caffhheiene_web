ca3c13f82fcfe335f908d1efb405439409856a29
SWARVY <swarvy0826@naver.com>
2023-11-16 15:54:32 +0900

    feat/refactor : 공방페이지 다크모드 적용, 디자인 변경

--- caffhheiene/src/containers/projects/PreviousProjects.tsx
+++ caffhheiene/src/containers/projects/PreviousProjects.tsx
@@ -1,19 +1,15 @@
-import Title from '@/components/Title'
+import SubTitle from '@/components/SubTitle'
 import { PREVIOUS_PROJECTS } from '@/constants/previousProjects'
 import { MAIN_TITLE, SUB_TITLE } from '@/constants/title'
 import ProjectItem from '@/containers/projects/ProjectItem'
-import { GamepadSVG } from '../../../public/svgs'
 
 export default function PreviousProjects() {
   return (
     <div className="grid gap-10">
-      <Title
-        svg={<GamepadSVG className="h-14 w-14" />}
-        title={MAIN_TITLE.projects}
-      />
-      <h1 className="rounded-md bg-white_hover p-3 shadow-md transition ease-in hover:bg-white_hover_weight">
+      <SubTitle title={MAIN_TITLE.projects} />
+      <h3 className="rounded-md bg-white_hover p-3 shadow-md transition ease-in hover:bg-white_hover_weight dark:bg-neutral-600 dark:text-white dark:hover:bg-neutral-500">
         {SUB_TITLE.notice}
-      </h1>
+      </h3>
       {PREVIOUS_PROJECTS.map((data, idx) => (
         <ProjectItem
           key={idx}
--- caffhheiene/src/containers/projects/ProjectItem.tsx
+++ caffhheiene/src/containers/projects/ProjectItem.tsx
@@ -22,14 +22,16 @@ export default function ProjectItem({
   return (
     <div className="grid gap-3">
       <a href={link} target="_blank" rel="noreferrer">
-        <h1 className="text-2xl font-bold">{name}</h1>
+        <h1 className="text-2xl font-bold text-black dark:text-white">
+          {name}
+        </h1>
       </a>
-      <h2 className="">{description}</h2>
+      <h2 className="text-black dark:text-gray-300">{description}</h2>
       <div className="overflow-hidden rounded-md shadow-md">
-        <div className="bg-white_hover transition ease-in hover:bg-white_hover_weight">
+        <div className="bg-white_hover transition ease-in hover:bg-white_hover_weight dark:bg-neutral-600 dark:text-white dark:hover:bg-neutral-500">
           <h1 className="p-3 font-bold">{SUB_TITLE.techStacks}</h1>
         </div>
-        <div className="grid grid-cols-2 gap-2 p-3 md:grid-cols-3">
+        <div className="grid grid-cols-2 gap-2 p-3 dark:bg-neutral-700 md:grid-cols-3">
           {stack.map((tech, idx) => (
             <Badge
               key={idx}

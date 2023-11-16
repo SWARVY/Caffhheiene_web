4bbc3e3f823e03adeb758537583b9cc611baca93
SWARVY <swarvy0826@naver.com>
2023-11-16 15:56:20 +0900

    refactor : 기본 글꼴 변경

--- caffhheiene/src/app/client.tsx
+++ caffhheiene/src/app/client.tsx
@@ -31,7 +31,7 @@ export default function Client({ children }: TransitionDivProps) {
   return (
     <AnimatePresence initial mode="wait">
       <motion.div
-        className="font-NanumBarunpen_Rg"
+        className="font-SeoulHangangLong_Lg"
         key={pathName}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}

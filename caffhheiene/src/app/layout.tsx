cfe96479c45a665a5fadfe8f7889c53c8347be4e
SWARVY <swarvy0826@naver.com>
2023-11-16 15:38:26 +0900

    feat : recoil 세팅 및 적용

--- caffhheiene/src/app/layout.tsx
+++ caffhheiene/src/app/layout.tsx
@@ -4,10 +4,11 @@ import BottomNavigator from '@/components/BottomNavigator'
 import Client from '@/app/client'
 import TopNavigator from '@/components/TopNavigator'
 import { type ReactNode } from 'react'
+import RecoilRootWrapper from '@/app/RecoilRootWrapper'
 
 export const metadata = {
-  title: 'Caffhheiene',
-  description: 'welcome! this is Caffhheiene',
+  title: '신현호의 기술서재',
+  description: '학습한 내용들을 기록합니다.',
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
@@ -19,14 +20,16 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           content="Cezz6dsFAjTiCwlqVouHHVYzFJ1rZnvdj8qmCvHm9MQ"
         />
       </head>
-      <body className="grid w-full">
-        <TopNavigator />
-        <Client>
-          <div className="mt-[9rem] grid justify-items-center">
-            <div className="w-full max-w-xl p-2">{children}</div>
-          </div>
-        </Client>
-        <BottomNavigator />
+      <body className="grid w-full bg-white transition duration-100 ease-in dark:bg-neutral-800">
+        <RecoilRootWrapper>
+          <TopNavigator />
+          <Client>
+            <div className="mt-[9rem] grid justify-items-center">
+              <div className="w-full max-w-3xl p-2">{children}</div>
+            </div>
+          </Client>
+          <BottomNavigator />
+        </RecoilRootWrapper>
       </body>
     </html>
   )

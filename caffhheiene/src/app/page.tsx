c89cb69e6e197a88bace729cde94cce0fa494beb
SWARVY <swarvy0826@naver.com>
2023-11-16 15:55:16 +0900

    refactor : 메인페이지 introduce 컴포넌트 추가

--- caffhheiene/src/app/page.tsx
+++ caffhheiene/src/app/page.tsx
@@ -1,13 +1,11 @@
-import { MAIN_TITLE } from '@/constants/title'
 import Carousel from '@/containers/main/Carousel'
+import Introduce from '@/containers/main/Introduce'
 import RecentlyPost from '@/containers/main/RecentlyPost'
 
 export default function Page() {
   return (
     <div className="grid space-y-20">
-      <h1 className="text-center font-MABINOGI_Classic text-7xl font-bold drop-shadow-lg md:text-8xl">
-        {MAIN_TITLE.welcome}
-      </h1>
+      <Introduce />
       <Carousel />
       <RecentlyPost />
     </div>

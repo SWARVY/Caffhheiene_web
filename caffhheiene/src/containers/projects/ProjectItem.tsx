e63d1710e53a421d8367e940d2a6b3c3aef109e6
SWARVY <swarvy0826@naver.com>
2023-11-18 21:30:14 +0900

    feat : 프로젝트 아이템 데코레이션 추가

--- caffhheiene/src/containers/projects/ProjectItem.tsx
+++ caffhheiene/src/containers/projects/ProjectItem.tsx
@@ -28,8 +28,13 @@ export default function ProjectItem({
       </a>
       <h2 className="text-black dark:text-gray-300">{description}</h2>
       <div className="overflow-hidden rounded-md shadow-md">
-        <div className="bg-white_hover transition ease-in hover:bg-white_hover_weight dark:bg-neutral-600 dark:text-white dark:hover:bg-neutral-500">
+        <div className="grid grid-cols-2 bg-white_hover transition ease-in hover:bg-white_hover_weight dark:bg-neutral-600 dark:text-white dark:hover:bg-neutral-500">
           <h1 className="p-3 font-bold">{SUB_TITLE.techStacks}</h1>
+          <div className="flex items-center justify-end gap-x-2 p-3">
+            <div className="h-3 w-3 rounded-full bg-red-600" />
+            <div className="h-3 w-3 rounded-full bg-yellow-500" />
+            <div className="h-3 w-3 rounded-full bg-green-600" />
+          </div>
         </div>
         <div className="grid grid-cols-2 gap-2 p-3 dark:bg-neutral-700 md:grid-cols-3">
           {stack.map((tech, idx) => (

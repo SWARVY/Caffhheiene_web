4f156924a4f8eb76e5a7d9a70427f4fa074c52ce
SWARVY <swarvy0826@naver.com>
2023-11-18 21:28:50 +0900

    refactor, fix : mdxComponent 파일 분리, toc 정상작동하도록 수정

---
+++ caffhheiene/src/containers/posts/detail/MdxComponents.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+import { type MDXComponents } from 'mdx/types'
+
+const mdxComponents: MDXComponents = {
+  a: ({ href, children }) => (
+    <Link
+      href={href as string}
+      className="text-base text-gray-400 no-underline transition duration-150 ease-in hover:text-black dark:text-gray-300 dark:hover:text-white">
+      {children}
+    </Link>
+  ),
+  strong: ({ children }) => (
+    <strong className="rounded-md bg-neutral-600 pb-0.5 pl-1 pr-1 pt-0.5 text-white">
+      {children}
+    </strong>
+  ),
+  blockquote: ({ children }) => (
+    <blockquote className="dark:text-white">{children}</blockquote>
+  ),
+  pre: ({ children }) => <pre className="p-0">{children}</pre>,
+  h1: ({ id, children }) => (
+    <h1
+      id={id}
+      className="hover:underline-offset-3 hover:underline dark:text-white">
+      {children}
+    </h1>
+  ),
+  h2: ({ id, children }) => (
+    <h2 id={id} className="dark:text-white">
+      {children}
+    </h2>
+  ),
+  h3: ({ id, children }) => (
+    <h3 id={id} className="dark:text-white">
+      {children}
+    </h3>
+  ),
+  h4: ({ id, children }) => (
+    <h3 id={id} className="dark:text-white">
+      {children}
+    </h3>
+  ),
+}
+
+export default mdxComponents
--- caffhheiene/src/containers/posts/detail/MdxRenderer.tsx
+++ caffhheiene/src/containers/posts/detail/MdxRenderer.tsx
@@ -1,36 +1,10 @@
 import { type Post } from '@/contentlayer/generated'
-import { type MDXComponents } from 'mdx/types'
 import { useMDXComponent } from 'next-contentlayer/hooks'
-import Link from 'next/link'
+import mdxComponents from './MdxComponents'
 import '../style/reset.css'
 
 interface MdxRendererProps {
   post: Post
-}
-
-const mdxComponents: MDXComponents = {
-  a: ({ href, children }) => (
-    <Link
-      href={href as string}
-      className="text-lg text-gray-400 no-underline transition duration-150 ease-in hover:text-black dark:text-gray-300">
-      {children}
-    </Link>
-  ),
-  strong: ({ children }) => (
-    <strong className="dark:text-white">{children}</strong>
-  ),
-  blockquote: ({ children }) => (
-    <blockquote className="dark:text-white">{children}</blockquote>
-  ),
-  pre: ({ children }) => <pre className="p-0">{children}</pre>,
-  h1: ({ children }) => (
-    <h1 className="hover:underline-offset-3 hover:underline dark:text-white">
-      {children}
-    </h1>
-  ),
-  h2: ({ children }) => <h2 className="dark:text-white">{children}</h2>,
-  h3: ({ children }) => <h3 className="dark:text-white">{children}</h3>,
-  h4: ({ children }) => <h3 className="dark:text-white">{children}</h3>,
 }
 
 export default function MdxRenderer({ post }: MdxRendererProps) {

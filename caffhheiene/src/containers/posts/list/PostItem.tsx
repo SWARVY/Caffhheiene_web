f035c4f0fdbc141918a6fb43e00f0701cb14fc8b
SWARVY <swarvy0826@naver.com>
2024-01-08 17:03:57 +0900

    feat : 포스트 내부와 리스트에 캘린더 아이콘 추가

--- caffhheiene/src/containers/posts/list/PostItem.tsx
+++ caffhheiene/src/containers/posts/list/PostItem.tsx
@@ -1,5 +1,6 @@
 import Tag from '@/containers/posts/Tag'
 import { type Post } from '@/contentlayer/generated'
+import { CalendarIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
 
 interface PostItemProps {
@@ -17,12 +18,15 @@ export default function PostItem({ id, post }: PostItemProps) {
           <div className="grid gap-3">
             <div className="grid gap-1">
               <div className="flex items-center justify-between">
-                <h3 className="truncate text-left text-2xl font-bold transition duration-200 ease-in group-hover:text-gray-600 dark:text-white dark:group-hover:text-white dark:group-hover:brightness-75">
+                <h3 className="w-3/4 truncate text-left text-2xl font-bold transition duration-200 ease-in group-hover:text-gray-600 dark:text-white dark:group-hover:text-white dark:group-hover:brightness-75">
                   {post.title}
                 </h3>
-                <h3 className="justify-end text-sm text-gray-400 dark:text-gray-300">
-                  {post.date}
-                </h3>
+                <div className="flex gap-x-1">
+                  <CalendarIcon className="h-4 w-4 items-center dark:fill-white" />
+                  <h3 className="justify-end text-sm text-gray-400 dark:text-gray-300">
+                    {post.date}
+                  </h3>
+                </div>
               </div>
               <p className="truncate text-lg text-gray-400 dark:text-gray-300">
                 {post.description}

4f71ab31ef979e17200c67c660dffac587bee0c2
SWARVY <swarvy0826@naver.com>
2024-01-18 21:34:40 +0900

    Revert "refactor : leagcy 파일들 삭제"
    This reverts commit 32862404258a8f2bc73de5f52c206bc721d8a02d.

---
+++ caffhheiene/legacy/MarkdownRenderer.tsx
@@ -0,0 +1,36 @@
+// 'use client';
+//
+// import customAtomDark from '@/containers/posts/style/customAtomDark';
+// import Markdown from 'react-markdown';
+// import remarkGfm from 'remark-gfm';
+// import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+// import '@/containers/posts/style/reset.css';
+//
+// interface MarkdownRendererProps {
+//   content: string;
+// }
+//
+// export default function MdRenderer({ content }: MarkdownRendererProps) {
+//   return (
+//     <Markdown
+//       className="markdown"
+//       remarkPlugins={[remarkGfm]}
+//       components={{
+//         code({ node, className, children, ...props }) {
+//           const match = /language-(\w+)/.exec(className ?? '');
+//           return match != null ? (
+//             <SyntaxHighlighter style={customAtomDark as any} PreTag="div" language={match[1]}>
+//               {String(children).replace(/\n$/, '')}
+//             </SyntaxHighlighter>
+//           ) : (
+//             <code className={className ?? ''} {...props}>
+//               {children}
+//             </code>
+//           );
+//         },
+//       }}
+//     >
+//       {content}
+//     </Markdown>
+//   );
+// }
---
+++ caffhheiene/legacy/TestSources/TestCarouselData.ts
@@ -0,0 +1,17 @@
+export const TestCarouselData = [
+  {
+    src: 'https://picsum.photos/4096/2048',
+    title: 'Test Title 1',
+    subtitle: 'Test Subtitle 1',
+  },
+  {
+    src: 'https://picsum.photos/4096/2048/',
+    title: 'Test Title 2',
+    subtitle: 'Test Subtitle 2',
+  },
+  {
+    src: 'https://picsum.photos/4096/2048//',
+    title: 'Test Title 3',
+    subtitle: 'Test Subtitle 3',
+  },
+];

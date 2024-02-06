72e1ce67549a3ad374e2cb80cdb7550d939298f0
SWARVY <swarvy0826@naver.com>
2024-02-06 22:23:44 +0900

    refactor : 페이지 리사이징에 따른 메인페이지 css 속성 추가

--- caffhheiene/src/containers/main/Introduce.tsx
+++ caffhheiene/src/containers/main/Introduce.tsx
@@ -4,7 +4,7 @@ import { USER } from '@/constants/user'
 
 export default function Introduce() {
   return (
-    <div className="grid gap-y-4 border-b pb-3">
+    <div className="grid w-full gap-y-4 border-b pb-3">
       <Title title={MAIN_TITLE.welcome} />
       <div className="flex items-center justify-start gap-x-2 pl-2">
         <h2 className="text-xl font-bold dark:text-white">{USER.name}</h2>

6562c04d929b7db08daf74a9c4dc9320e3a8ab12
SWARVY <swarvy0826@naver.com>
2024-03-10 22:42:24 +0900

    feat : 다크모드의 기본값을 시스템 값으로 변경

--- caffhheiene/src/app/client.tsx
+++ caffhheiene/src/app/client.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import darkModeState from '@/atom/darkMode'
 import { AnimatePresence, motion } from 'framer-motion'
 import { usePathname } from 'next/navigation'
-import { type ReactNode } from 'react'
+import { useEffect, type ReactNode } from 'react'
+import { useSetRecoilState } from 'recoil'
 
 interface TransitionDivProps {
   children: ReactNode
@@ -10,6 +12,14 @@ interface TransitionDivProps {
 
 export default function Client({ children }: TransitionDivProps) {
   const pathName = usePathname()
+  const setDarkMode = useSetRecoilState(darkModeState)
+
+  useEffect(() => {
+    setDarkMode(
+      window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+    )
+  })
 
   return (
     <AnimatePresence initial mode="wait">

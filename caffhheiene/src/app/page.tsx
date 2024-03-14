c33f76c36a84b67daa58c9e41414763d8346743c
SWARVY <swarvy0826@naver.com>
2024-03-14 20:39:35 +0900

    refactor : 반응형 padding 사이즈 조정

--- caffhheiene/src/app/page.tsx
+++ caffhheiene/src/app/page.tsx
@@ -3,7 +3,7 @@ import RecentlyPost from '@/containers/main/RecentlyPost'
 
 export default function Page() {
   return (
-    <section className="grid w-full max-w-3xl gap-y-20 p-5 xl:p-0">
+    <section className="grid w-full max-w-3xl gap-y-20 p-5 md:p-2 xl:p-0">
       <Introduce />
       <RecentlyPost />
     </section>

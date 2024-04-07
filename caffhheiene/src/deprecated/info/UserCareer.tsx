d70e1cbe48bfb9b643472fd172cf37fc5e38d258
SWARVY <swarvy0826@naver.com>
2024-04-07 23:04:41 +0900

    feat : deprecated된 로직들 주석처리

--- caffhheiene/src/deprecated/info/UserCareer.tsx
+++ caffhheiene/src/deprecated/info/UserCareer.tsx
@@ -1,16 +1,16 @@
-import { USER_CAREER } from '@/constants/user'
+// import { USER_CAREER } from '@/constants/user'
 
-export default function UserCareer() {
-  return (
-    <div className="w-full flex-col space-y-1 p-2">
-      {USER_CAREER.map((data) => (
-        <div className="grid grid-cols-2 gap-x-3">
-          <p className="text-sm text-gray-500 dark:text-gray-400 sm:text-base">
-            {data.date}
-          </p>
-          <p className="text-sm dark:text-white sm:text-base">{data.name}</p>
-        </div>
-      ))}
-    </div>
-  )
-}
+// export default function UserCareer() {
+//   return (
+//     <div className="w-full flex-col space-y-1 p-2">
+//       {USER_CAREER.map((data) => (
+//         <div className="grid grid-cols-2 gap-x-3">
+//           <p className="text-sm text-gray-500 dark:text-gray-400 sm:text-base">
+//             {data.date}
+//           </p>
+//           <p className="text-sm dark:text-white sm:text-base">{data.name}</p>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }

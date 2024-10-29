63466256d95c5504c20620551075c522b748689c
SWARVY <swarvy0826@naver.com>
2024-10-29 19:17:14 +0900

    feat: `Google Analytics` 추가 및 환경변수 분리

---
+++ src/utils/GoogleAnalytics.tsx
@@ -0,0 +1,25 @@
+import Script from 'next/script'
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js
+				?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        dangerouslySetInnerHTML={{
+          __html: `
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+		`,
+        }}
+      />
+    </>
+  )
+}
